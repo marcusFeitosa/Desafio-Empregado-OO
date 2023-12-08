ed71f40c7b98b63fa11909fb6cb906403667b820
marcusFeitosa <marcus.feitosa_al@hotmail.com>
2023-12-07 22:46:55 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,69 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Address;
+import entities.Department;
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc=new Scanner(System.in);
+		
+		
+		System.out.print("Nome do departamento: ");
+		String nomeDepartamento=sc.next();
+		System.out.print("Dia do pagamento: ");
+		int diaPagamento=sc.nextInt();
+		System.out.print("Email: ");
+		String email=sc.next();
+		System.out.print("Telefone: ");
+		String telefone=sc.next();
+		
+		Department dep=new Department(nomeDepartamento, diaPagamento, new Address(email,telefone));
+		
+		System.out.print("Quantos funcionarios tem o departamento: ");
+		int quant=sc.nextInt();
+		
+		for(int i=0;i<quant;i++) {
+			System.out.println("Dados do funcionario "+(i+1)+":");
+			System.out.print("Nome: ");
+			sc.nextLine();
+			String nome=sc.nextLine();
+			System.out.print("Salario: ");
+			double salario=sc.nextDouble();
+			dep.addEmployee(new Employee(nome,salario));
+		}
+		    // pulando uma linha
+		System.out.println();
+		
+		
+		// mostrando o relatorio
+		showReport(dep);
+			
+	
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+	private static void showReport(Department dep) {
+		int n=dep.getEmployees().size();
+		
+		System.out.println("FOLHA DE PAGAMENTO:");
+		System.out.println("Departamento de vendas: "+dep.payroll());
+		System.out.println("Pagamento realizado no dia "+dep.getPayDay());
+		System.out.println("Funcionarios");
+		for(int i=0;i<n;i++)
+			System.out.println(dep.getEmployees().get(i).getName());
+		System.out.print("Para dúvidas favor entrar em contato: "+dep.getAddress().getEmail());
+	}
+
+}
---
+++ src/entities/Address.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class Address {
+	
+	private String email;
+	private String phone;
+	
+	Address(){
+		
+	}
+
+	public Address(String email, String phone) {
+		this.email = email;
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+
+}
